40633316ba040d9bfab3f4677e6bd9c753625677
sounritesh <sounritesh@gmail.com>
2022-01-05 21:15:48 +0530

    sample commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Mindshift
-Contains all work related to Mindshift Data Science Internship
+
+```
+pip install -r requirements.txt
+```
+```
+python run.py --file 6-dec.RDS
+```
--- run.py
+++ run.py
@@ -1,15 +1,25 @@
 from argparse import ArgumentParser
 from datetime import date
 
+from modules.data import get_df_from_rds
+
 def main():
     parser = ArgumentParser()
 
     parser.add_argument("--idling", help="To run idling algorithm", action="store_true")
     parser.add_argument("--drain", help="To run drain algorithm", action="store_true")
 
-    parser.add_argument("-s", "--sites", nargs='+', help="The name of sites: {bagru1, bagru2, dand, jobner, sawarda}", required=True)
-    parser.add_argument("--start", type=date.fromisoformat(), help="The starting date for data")
-    parser.add_argument("--end", type=date.fromisoformat(), help="The ending date for data")
+    # parser.add_argument("-s", "--sites", nargs='+', help="The name of sites: {bagru1, bagru2, dand, jobner, sawarda}", required=True)
+    parser.add_argument("--start", type=date.fromisoformat, help="The starting date for data")
+    parser.add_argument("--end", type=date.fromisoformat, help="The ending date for data")
+
+    parser.add_argument("--file")
+
+    args = parser.parse_args()
+
+    df = get_df_from_rds('sample_data/' + args.file)
+    df.to_csv('sample.csv', index=False)
+
 
 if __name__=='__main__':
     main()
