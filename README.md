063b08f0b1eae89481918d2bc7b88293140281b4
Manjima Dutta <manjimadutta1998@gmail.com>
2023-08-01 16:57:05 +0530

    First commit in this branch

--- README.md
+++ README.md
@@ -8,12 +8,12 @@ pip install -r requirements.txt
 1. If you want to generate Three algorithm files for all termids, put the command this way in your terminal, 
 
 ```
->> python run.py -IA '_your input allmods file path_' -IC '_your input cst file path' -t  -O '_output folder path_'
+python run.py -IA '_your input allmods file path_' -IC '_your input cst file path'  -O '_output folder path_' -t 
 ```
 
 2. If you have specific termids to run the algorithms for, put the command this way, (put space-separated termids after '-t')
 
 ```
->> python run.py -IA '_your input allmods file path_' -IC '_your input cst file path' -t 1204000785 1204000506 -O '_output folder path_'
+python run.py -IA '_your input allmods file path_' -IC '_your input cst file path' -O '_output folder path_' -t 1204000785 1204000506
 ```
 
--- modules/algorithms.py
+++ modules/algorithms.py
@@ -23,27 +23,22 @@ def round_to_nearest(number, nearest):
     else:
         return number
 
-def internal_agg(input_df,input_mods_df,dist,n,termid): 
+def internal_agg(df,mods_df,dist,n,termid): 
 
-    df,mods_df = data_prep_distance(input_df,input_mods_df)
+    # df,mods_df = data_prep_distance(input_df,input_mods_df)
+    # df = df.reset_index(drop=True)
     list_=[]
-    max_ = round_to_nearest(df['cumsum_dist_Sir'].max(),n)
+    max_ = round_to_nearest(df['cumsum_dist_Sir'].max(),n)+dist
     for i in range(dist,max_,dist):
         temp_dict = {}
         sample = df[(df['cumsum_dist_Sir']>=(i-dist))&(df['cumsum_dist_Sir']<=i)]
         if len(sample)!=0:
-            temp_dict['termid'] = sample['termid'].tolist()[0]
-            temp_dict['regNumb'] = sample['regNumb'].tolist()[0]
-            temp_dict['Class'] = termid_class_map[str(termid)]
-            temp_dict['total_samples'] = len(sample)
-            temp_dict['start_time'] = sample.head(1)['ts'].item()
-            temp_dict['end_time'] = sample.tail(1)['ts'].item()
-            temp_dict['Initial_level'] = sample.head(1)['fuel'].item()
-            temp_dict['End_level'] = sample.tail(1)['fuel'].item()
-            temp_dict['Refuel_sum'] = sample['REfuel_unique'].sum()
             total_time = sample['time_diff'].sum();ig_time = sample.query("currentIgn==1")['time_diff'].sum()
-            temp_dict['total_dist'] = sample['disthav'].sum()
-            temp_dict['ig_perc'] = ig_time/total_time
+            keys=['termid','regNumb','Class','total_samples','start_time','end_time','Initial_level','End_level','Refuel_sum','total_dist','ig_perc']
+            values = [sample['termid'].tolist()[0],sample['regNumb'].tolist()[0],termid_class_map[str(termid)],len(sample),
+            sample.head(1)['ts'].item(),sample.tail(1)['ts'].item(),sample.head(1)['fuel'].item(),sample.tail(1)['fuel'].item(),sample['REfuel_unique'].sum(),
+            sample['disthav'].sum(),ig_time/total_time]
+            temp_dict.update(zip(keys,values))
             list_.append(temp_dict)
         else:
             pass
@@ -55,18 +50,19 @@ def distance_algo(input_df,input_mods_df,termid):
     df,mods_df = data_prep_distance(input_df,input_mods_df)   #df_1 , mods_df_1
     if termid_class_map[str(termid)] == 'high_movement':
         list_1 = internal_agg(df,mods_df,1000,1000,termid)
+        dist_df = pd.DataFrame(list_1)
+        dist_df['Fuel_cons'] = dist_df['Initial_level'] - dist_df['End_level']
+        dist_df['Newf_Fuel_cons'] = dist_df['Fuel_cons']+dist_df['Refuel_sum']
+        dist_df['Total_time'] = (dist_df['end_time']-dist_df['start_time']).dt.total_seconds()/60
+        dist_df['lph'] = (dist_df['Fuel_cons']/dist_df['Total_time'])*60
+        dist_df['Median_lph'] = dist_df['lph'].median()
+        dist_df['lp_100'] = (dist_df['Fuel_cons']/dist_df['total_dist'])*100000
+        dist_df['Median_lp100'] = dist_df['lp_100'].median()
     else:
-        list_1 = internal_agg(df,mods_df,100,100,termid)
-    dist_df = pd.DataFrame(list_1)
-    dist_df['Fuel_cons'] = dist_df['Initial_level'] - dist_df['End_level']
-    dist_df['Newf_Fuel_cons'] = dist_df['Fuel_cons']+dist_df['Refuel_sum']
-    dist_df['Total_time'] = (dist_df['end_time']-dist_df['start_time']).dt.total_seconds()/60
-    dist_df['lph'] = (dist_df['Fuel_cons']/dist_df['Total_time'])*60
-    dist_df['Median_lph'] = dist_df['lph'].median()
-    dist_df['lp_100'] = (dist_df['Fuel_cons']/dist_df['total_dist'])*100000
-    dist_df['Median_lp100'] = dist_df['lp_100'].median()
+        dist_df = pd.DataFrame()
+    
 
-    return dist_df
+    return dist_df    #df,mods_df,
 
 def hour_algo(input_df,input_mods_df):
 
